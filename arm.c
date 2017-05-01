a7ed9793d7dcfb3b190a99d5e04b34d5438154b0
Pradeestark <Pradeestarck@gmail.com>
2017-05-01 13:53:43 +0530

    Create arm.c

---
+++ arm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+ {
+ int a,n,b=0,t;
+ printf("Enter the no: ");
+ scanf("%d",&n);
+ t=n;
+ while(n>0)
+ {
+  a=n%10;
+  b=b+a*a*a;
+  n=n/10;
+ }
+ if(b==t)
+  {
+    printf("Its an Armstrong number");
+  }
+ else
+  {
+    printf("Its Not an Armstrong number");
+  }
+ return 0;
+}
