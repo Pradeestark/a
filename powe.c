08aeb115ad50298e870d534dccd22953ac6dfc6a
Pradeestark <Pradeestarck@gmail.com>
2017-05-01 18:55:51 +0530

    Create powe.c

---
+++ powe.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int base,exp,value=1;
+printf("Enter the base value and power value");
+scanf("%d%d",&base,&exp);
+while(exp!=0);
+{
+value*=base;
+--exp;
+}
+printf("value=%d",value);
+}
